0d27865d370b05467f889e77faf7fe792b95efc3
Steve B <bedell.369@osu.edu>
2019-06-12 17:15:58 -0400

    Adding Firebase write functionality

---
+++ src/firebase-shell.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase';
+
+const config = {
+  // Grab config from your Firebase console settings
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
--- src/fun-food-friends.js
+++ src/fun-food-friends.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './fun-food-friends.css';
+import firebase from './firebase.js';
 
 class FunFood extends Component {
   constructor() {
@@ -12,24 +13,27 @@ class FunFood extends Component {
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
     return (
       <div className='app'>
         <header>
-            <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
-            </div>
+          <div className='wrapper'>
+            <h1>Fun Food Friends</h1>
+          </div>
         </header>
+
         <div className='container'>
           <section className='add-item'>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
               <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
               <button>Add Item</button>
             </form>
           </section>
+          
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
@@ -45,6 +49,31 @@ class FunFood extends Component {
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // Add to Firebase Database table called "items":
+    const item = {
+      itemName: this.state.currentItem,
+      user: this.state.username
+    }
+
+    // Add a new document with a generated id.
+    firebase.firestore().collection("items").add(item)
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+
+    // clear inputs after submitting:
+    this.setState({
+      currentItem: '',
+      username: ''
     });
   }
 }
