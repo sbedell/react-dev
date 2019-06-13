ec41a4fce17575eae79c751b59ee747da0083510
Steve B <bedell.369@osu.edu>
2019-06-13 18:26:10 -0400

    Displaying items on page and implementing delete functionality

--- src/fun-food-friends.js
+++ src/fun-food-friends.js
@@ -9,7 +9,8 @@ class FunFood extends Component {
 
     this.state = {
       currentItem: '',
-      username: ''
+      username: '',
+      items: []
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -37,12 +38,66 @@ class FunFood extends Component {
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
+                {this.state.items.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <h3>{item.itemName}</h3>
+                      <p>brought by: {item.user}</p>
+                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    let itemsRef = firebase.firestore().collection("items");
+
+    // TODO - Fix this...when I add new items to this, it doesn't populate it correctly on the page. 
+    // It only shows the most recently added item, until the user refreshes the page.
+    itemsRef.onSnapshot(snapshot => {
+      console.log("Received snapshot: ", snapshot);
+
+      // Attempt to set state:
+      let newState = [];
+      
+      let items = snapshot.docChanges();
+      items.forEach(item => {
+        // console.log("item: ", item);
+
+        newState.push({
+          id: item.doc.id,
+          itemName: item.doc.data().itemName,
+          user: item.doc.data().user
+        });
+      });
+
+      this.setState({
+        items: newState
+      });
+
+      // Just do some console logging for sanity sake
+      snapshot.docChanges().forEach(change => {
+        if (change.type === "added") {
+          console.log("Added: ", change.doc.data());
+        }
+        
+        if (change.type === "modified") {
+          console.log("Modified: ", change.doc.data());
+        }
+        
+        if (change.type === "removed") {
+          console.log("Removed: ", change.doc.data());
+        }
+      });
+    }, err => {
+      console.error("Encountered error: ", err);
+    });
   }
 
   // Generic function to handle changes to input boxes
@@ -55,13 +110,12 @@ class FunFood extends Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    // Add to Firebase Database table called "items":
     const item = {
       itemName: this.state.currentItem,
       user: this.state.username
     }
 
-    // Add a new document with a generated id.
+    // Add a new document with a generated id to "items" table/db.
     firebase.firestore().collection("items").add(item)
       .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
@@ -70,11 +124,15 @@ class FunFood extends Component {
         console.error("Error adding document: ", error);
       });
 
-    // clear inputs after submitting:
+    // clear inputs after successful submit:
     this.setState({
       currentItem: '',
       username: ''
     });
+  }
+
+  removeItem(itemId) {
+    firebase.firestore().collection("items").doc(itemId).delete();
   }
 }
 
