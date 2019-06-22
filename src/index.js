a347985a31c34add72163e3cd6e853dbfe49512d
Steve B <bedell.369@osu.edu>
2019-06-22 18:53:32 -0400

    Adding react-router-dom to project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './index.css';
-// import App from './App';
+
 import FunFood from './fun-food-friends';
+import Login from './pages/login';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<FunFood />, document.getElementById('root'));
+// Reference - https://codeburst.io/getting-started-with-react-router-5c978f70df91
+const routing = (
+  <Router>
+    <Route exact path="/" component={FunFood} />
+    <Route path="/login" component={Login} />
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
