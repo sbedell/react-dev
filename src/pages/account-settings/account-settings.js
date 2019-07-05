16678320e047a17ade80cc6c61205f81f338d5dc
Steve B <bedell.369@osu.edu>
2019-07-05 17:19:59 -0400

    Updating account-settings js and CSS - ability to update your username now! :D

--- src/pages/account-settings/account-settings.js
+++ src/pages/account-settings/account-settings.js
@@ -18,8 +18,14 @@ class AccountSettings extends React.Component {
     super();
 
     this.state = {
-      currentUser: null
+      currentUser: null,
+      editEmail: false,
+      editUsername: false
     };
+
+    this.openEditMode = this.openEditMode.bind(this);
+    this.saveUsername = this.saveUsername.bind(this);
+    this.cancelEdit = this.cancelEdit.bind(this);
   }
 
   render() {
@@ -28,17 +34,29 @@ class AccountSettings extends React.Component {
         <h1>Profile / Account Info</h1>
 
         {/* TODO - Stuff to display: DisplayName, Email, Email Verified (y/n) */}
-            
-        {/* <label htmlFor="user-email">Email</label>
-        <input id="user-email" type="email"></input> */}
         <div className="profile-info-section">
-          <label>Email: </label>
+          <label htmlFor="user-email">Email: </label>
           {this.state.currentUser ? 
             <span>{this.state.currentUser.email}</span>
             :
-            <span>-</span>
+            <span>---</span>
           }
-          <button>Edit Email</button>
+          {this.state.editEmail ?
+            <div className="edit-section">
+              <label>New Email:</label>
+              <input id="user-email" type="email"></input>
+              
+              <label>Confirm New Email:</label>
+              <input type="email"></input>
+              
+              <label>Current Password:</label>
+              <input type="password"></input>
+              <button>Save New Email</button>
+              <button name="cancel-edit-email" onClick={this.cancelEdit}>Cancel</button>
+            </div>
+            :
+            <button id="edit-email-btn" onClick={this.openEditMode}>Edit Email</button>
+          }
         </div>
 
         <div className="profile-info-section">
@@ -48,15 +66,32 @@ class AccountSettings extends React.Component {
           {this.state.currentUser ? 
             <span>{this.state.currentUser.displayName}</span>
             :
-            <span>-</span>
+            <span>---</span>
           }
-          <button>Edit Username</button>
+          {this.state.editUsername ?
+            <div className="edit-section">
+              <label>New Username:</label>
+              <input type="text" id="username-input"></input>
+              
+              <button name="save-username" onClick={this.saveUsername}>Save New Username</button>
+              <button name="cancel-edit-username" onClick={this.cancelEdit} type="button">Cancel</button>
+            </div>
+            :
+            <button id="edit-username-btn" type="button" onClick={this.openEditMode}>Edit Username</button>
+          }
         </div>
         
         <div className="profile-info-section">
-          <label htmlFor="user-password">Password</label>
-          <input id="user-password" type="password"></input>
+          <label>Set New Password</label>
+          {/* TODO - For this, need to open 3 fucking inputs, 1 for old password (then send to re-verify function),
+          and then one for new pass, another one to verify that new pass (check if they match). */}
+          <button>Create New Password</button>
         </div>
+
+        {/* TODO - make this a red button, etc, force user to input old password before doing this too */}
+        {/* <div className="profile-info-section">
+          <button>Delete Account?</button>
+        </div> */}
       </div>
     );
   }
@@ -71,23 +106,71 @@ class AccountSettings extends React.Component {
     }
   }
 
+  openEditMode(e) {
+    // console.log("OpenEditMode, e.target: ", e.target.id);
+
+    if (e.target.id === "edit-email-btn") {
+      this.setState({
+        editEmail: true
+      });
+    } else if (e.target.id === "edit-username-btn") {
+      this.setState({
+        editUsername: true
+      });
+    }
+  }
+
+  cancelEdit(e) {
+    console.log("cancelEdit. e.target.name: ", e.target.name);
+
+    if (e.target.name === "cancel-edit-username") {
+      this.setState({
+        editUsername: false
+      });
+    }
+  }
+
   /**
+   * Remember to setState editEmail to false after saving
+   */
+  // saveEmail() {
+  // }
+
+  /**
+   * Update / Save new username in the user's Firebase profil
+   * 
    * From Firebase docs: 
    * "You can update a user's basic profile information - 
    * the user's display name and profile photo URL - with the updateProfile method."
    */
-  updateProfile(newDisplayName, newPhotoUrl) {
-    let user = firebase.auth().currentUser;
-    let newProfileInfo = {
-      displayName: "",
-      photoUrl: ""
-    };
+  saveUsername(e) {
+    // console.log("saveUsername. e.target: ", e.target);
+    let newUsername = document.getElementById("username-input").value.trim();
 
-    user.updateProfile(newProfileInfo).then(() => {
-      // Update successful.
-    }).catch(error => {
-      console.error("Error updating profile: ", error);
-    });
+    if (newUsername.match(/\w+/)) {
+      // console.log("newUsername = ", newUsername);
+
+      let user = firebase.auth().currentUser;
+
+      let newProfileInfo = {
+        displayName: newUsername,
+      };
+
+      console.log("Updating profile with newProfileInfo: ", newProfileInfo);
+
+      user.updateProfile(newProfileInfo).then(() => {
+        console.log("Successfully updating profile - updated username");
+
+        // reset the input back to empty string:
+        document.getElementById("username-input").value = "";
+
+        this.setState({
+          editUsername: false
+        });
+      }).catch(error => {
+        console.error("Error updating profile: ", error);
+      });
+    }
   }
 
   updateEmail(newEmailAddr) {
