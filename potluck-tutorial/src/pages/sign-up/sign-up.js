7c8ecbb01c7b9978782fc78af369645d2b5e0a48
Steve B <bedell.369@osu.edu>
2019-07-12 16:51:34 -0400

    Basic work on sign-up page

--- potluck-tutorial/src/pages/sign-up/sign-up.js
+++ potluck-tutorial/src/pages/sign-up/sign-up.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import firebase from '../../firebase.js';
 
+import "./sign-up.css";
+
+/**
+ * TODO
+ * 1. Input boxes for username, email, password, button for "Sign Up"
+ */
+
 class SignUpPage extends React.Component {
   // constructor() {
   //   super();
 
   //   this.state = {
-  //     currentUser: null
+  //     stateprop: null
   //   };
   // }
 
   render() {
     return (
-      <h1>Sign Up Page</h1>
+      <main className="sign-up-page">
+        <h1>Sign Up</h1>
+        <h2>Register For a new account</h2>
+
+        <div className="user-input-section">
+          <label>Email:</label>
+          <input type="email"></input>
+        </div>
+
+        <div className="user-input-section">
+          <label>Password:</label>
+          <input type="password"></input>
+        </div>
+
+        <div className="user-input-section">
+          <label>Confirm Password:</label>
+          <input type="password"></input>
+        </div>
+
+        <div className="user-input-section">
+          <label>Username (optional, not used for logging in): </label>
+          <input type="text"></input>
+        </div>
+
+        <button>Create Account</button>
+      </main>
     );
   }
 }
@@ -21,7 +53,7 @@ class SignUpPage extends React.Component {
  * Call this when the user click "Sign up" or something, after filling in a username/email and password
  */
 // eslint-disable-next-line
-function signUp(email, password, username = "") {
+function signUp(email, password) {
   if (email && password) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(() => {
