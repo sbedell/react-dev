6b9e6e0209744690f5e64c1791787ed25f10b19d
Steve B <bedell.369@osu.edu>
2019-07-15 14:12:41 -0400

    Minor update on modal

--- potluck-tutorial/src/components/modal/modal.js
+++ potluck-tutorial/src/components/modal/modal.js
@@ -1,13 +1,17 @@
 /**
  * Using some code from:
  * https://alligator.io/react/modal-component/
+ * 
+ * Some tips on Functional vs. Class components: 
+ * https://guide.freecodecamp.org/react/functional-components-vs-class-components/
  */
 
 import React from 'react';
 
 import "./modal.css";
 
-const Modal = (props) => {
+// "Function" version
+function Modal(props) {
   const showHideClassName = props.show ? "modal display-block" : "modal display-none";
 
   return (
@@ -21,3 +25,24 @@ const Modal = (props) => {
 };
 
 export default Modal;
+
+/* Class version:
+class Modal extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
+
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-main">
+          <button className="close-btn" onClick={this.props.handleClose}>x</button>
+          { this.props.children }
+        </section>
+      </div>
+    );
+  }
+}
+*/
